30814ad2dbbb706aeb24615a7ddd199ed196ef1c
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-07-08 12:38:44 +0200

    chore: add other tasks

---
+++ 2_Python_Conditional_Statements/zad5.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Input I number: "))
+num_2 = int(input("Input II number: "))
+
+if (num_1 %2 == 0) or (num_2 % 2 == 0):
+    print("One of the following numbers is even")
+else:
+    print("Both of numbers are odd")
