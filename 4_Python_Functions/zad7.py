4cb907f7049f916ffab50712155ff60e0cecd9d8
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-07-18 14:40:11 +0200

    feat: add new task 7 from functions

--- 4_Python_Functions/zad7.py
+++ 4_Python_Functions/zad7.py
@@ -5,8 +5,17 @@ def angle_from_time(**kwargs) -> float:
     """
     Return the angle between pointers on the clock
     """
+    value_hour = kwargs["hour"]
+    value_minute = kwargs["minute"]
 
-    return kwargs
+    minute_angle = value_minute * 6
+
+    if value_hour >= 12:
+        hour_angle = (value_hour - 12) * 30 + (0.5 * value_minute)
+    else:
+         hour_angle = value_hour * 30 + (0.5 * minute_angle)
+
+    return abs(hour_angle - minute_angle)
 
 
 def main():
--- 4_Python_Lists/zad2.py
+++ 4_Python_Lists/zad2.py
@@ -4,9 +4,7 @@ score = [12, 1, 45, 76, 50, 23]
 
 for number in score:
 
-    if 1 <= number <= 49:
-        pass
-    else:
+    if not 1 <= number <= 49:
         new_number = random.randint(1, 49)
         print(f"Incorrect value {number} has been changed to {new_number}")
         number = new_number
