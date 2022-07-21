86737da8a2f1dbc057eb926666c048e366a77b52
“MGasiorowskii” <gasiorowskii.mateusz@gmail.com>
2022-07-21 23:02:43 +0200

    feat: add new task 6 from classes

--- 4_Python_Files/zad9.py
+++ 4_Python_Files/zad9.py
@@ -1,11 +1,31 @@
 import json
-# TODO "dn", "Description", "SPEED", "MTU",
 
 
-print("{}".format('DN'))
+intro ="""
+Interface Status
+================================================================================
+DN                                                 Description           Speed    
+MTU  
+"""
+
 
 with open("data.json") as f:
     data = json.load(f)
 
 for row in data['imdata']:
-    print(row['l1PhysIf']['attributes']['dn'])
+    dn = row['l1PhysIf']['attributes']['dn']
+    description = row['l1PhysIf']['attributes']['descr']
+    speed = row['l1PhysIf']['attributes']['speed']
+    mtu = row['l1PhysIf']['attributes']['mtu']
+
+    # print(intro)
+    print("Interface Status")
+    print("=" * 80)
+    print("DN", end="")
+    print(" " * 49, end="")
+    print("Description", end="")
+    print(" " * 11, end="")
+    print("Speed")
+    print("{:<7}{:>11}{:>25}\n{}\n".format(dn, description, speed, mtu))
+
+
